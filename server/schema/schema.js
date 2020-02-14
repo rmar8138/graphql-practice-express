125cda8fdbc1bd5d9277a759bb9f6989e9213c31
Ragan Martinez <ragan.martinez@live.com>
2020-02-14 16:33:24 +1100

    Update resolve functions

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -26,9 +26,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        // return _.find(authors, {
-        //   id: parent.authorId
-        // });
+        return Author.findById(parent.authorId);
       }
     }
   })
@@ -49,9 +47,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        // return _.filter(books, {
-        //   authorId: parent.id
-        // });
+        return Book.find({ authorId: parent.id });
       }
     }
   })
@@ -68,10 +64,7 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parent, args) {
-        // // code to get data from db
-        // return _.find(books, {
-        //   id: args.id
-        // });
+        return Book.findById(args.id);
       }
     },
     author: {
@@ -82,21 +75,19 @@ const RootQuery = new GraphQLObjectType({
         }
       },
       resolve(parent, args) {
-        // return _.find(authors, {
-        //   id: args.id
-        // });
+        return Author.findById(args.id);
       }
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        // return books;
+        return Book.find();
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        // return authors;
+        return Author.find();
       }
     }
   }
