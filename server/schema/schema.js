1f145878386338a5e29ead30c8d9b4f7182eedbe
Ragan Martinez <ragan.martinez@live.com>
2020-02-14 16:49:18 +1100

    Require non null fields in mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,7 +5,8 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLID,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
 const _ = require("lodash");
 const Book = require("./../models/book");
@@ -100,10 +101,10 @@ const Mutation = new GraphQLObjectType({
       type: AuthorType,
       args: {
         name: {
-          type: GraphQLString
+          type: new GraphQLNonNull(GraphQLString)
         },
         age: {
-          type: GraphQLInt
+          type: new GraphQLNonNull(GraphQLInt)
         }
       },
       resolve(parent, args) {
@@ -118,13 +119,13 @@ const Mutation = new GraphQLObjectType({
       type: BookType,
       args: {
         name: {
-          type: GraphQLString
+          type: new GraphQLNonNull(GraphQLString)
         },
         genre: {
-          type: GraphQLString
+          type: new GraphQLNonNull(GraphQLString)
         },
         authorId: {
-          type: GraphQLID
+          type: new GraphQLNonNull(GraphQLID)
         }
       },
       resolve(parent, args) {
